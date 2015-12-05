770f470e5932c653581a7bb94cec4d7ffd1fe66e
eliezerwohl <eliezerwohl@gmail.com>
2015-12-05 18:44:32 -0500

    inverted navbar, added function to webcoding button

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -48,17 +48,33 @@ $("document").ready(function(){
     $(".jumbotron").hide();
     picture.attr("src", "img/eliezerTrainer.jpg");
     var panelheading = $("<div>").addClass("panel-heading")
-    var trainingID =$("<div>").addClass("trainingID")
     var panel= $("<div>").addClass("panel panel-default")
     var panelbody = $("<div>").addClass("panel-body")
+    var trainingID =$("<div>").addClass("trainingID")
+    var panelheadingTest = $("<div>").addClass("panel-heading")
+    var panelTest= $("<div>").addClass("panel panel-default")
+    var panelbodyTest = $("<div>").addClass("panel-body")
 
 
     $(panelheading).append("Certifications")
     $(panelbody).append('<h3>NASM 2014-2016 <br><br> NPTI 2013- <br><br> Red Cross CPR AED 2013- </h3>')
-
     $(panel).append(panelheading);
     $(panel).append(panelbody).append;
-    $(".target").append(panel).append(trainingID);
+    $(panelheadingTest).append("Testamonials")
+    $(panelbodyTest).append('<h3>Put in the nice things ppl say about me here</h3>')
+    $(panelTest).append(panelheadingTest);
+    $(panelTest).append(panelbodyTest).append;
+    $(".target").append(panel).append(trainingID).append(panelTest);
+   });// end of personal training section
+    // web coding
+  $("#webCoding").on("click", function() {
+    if ( $( ".webCodingID" ).length ) {
+      return false;
+      }
+    $(".target").empty();
+    $(".jumbotron").hide();
+    picture.attr("src", "img/eliezerTrainer.jpg");
 
+    
   });
 });
