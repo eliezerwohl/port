a7d2de5971add229fc2428751239027cc6bf7e12
eliezerwohl <eliezerwohl@gmail.com>
2015-12-08 10:56:23 -0500

    made the life story shorter

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -35,11 +35,11 @@ $("document").ready(function(){
     picture.attr("src", "img/computer.jpg");
     var storyHeadline =$("<h3>").append("My Story")
     var strangelove =$("<h4>").append("or how I learned to stop worrying and love the code")
-    var storyParagraph1=$("<p>").append('For the past 3 years, I have been employed as a personal trainer.  One day, while talking to a client about the possible dangers of applying too much thermal paste after changing my CPU fan, he looked at me and said “why aren’t you working with computers?  have you thought about coding?” I told him I had, but never followed through with it. He told me he has recently changed careers, and started by taking online classes, and he thought I should do the same.  At first I was afraid.  What if I was wasting my time?  What if I broke the internet?')
+    var storyParagraph1=$("<p>").append('For the past 3 years, I have been employed as a personal trainer. One day, while talking to a client about the possible dangers of applying too much thermal paste on my CPU, he looked at me and said “why aren’t you working with computers? have you thought about coding?” I told him I had, but never followed through with it. He told me he has recently changed careers, and started by taking online classes, and he thought I should do the same. ')
     
-    var storyParagraph2=$("<p>").append('So started take Code Academy classes.  And I really enjoyed it.  But I soon hit a wall.  I completed a HTML and CSS course, but never felt like I really knew what I was doing.  So I gave up on it for a few months, unsure of my next step.')
-    var storyParagraph3=$("<p>").append('Until one Saturday night, I was hanging out with my friend.  He mentioned he had seen an ad for a  Rutgers Coding Bootcamp, and I was immediately intrigued.  I had looked at other bootcamps before, but this one was part-time.  I told my friend "I needed to think about it."')
-    var storyParagraph4=$("<p>").append('By the end of the next week I had signed up for the bootcamp.  A month later, I went to the first class.  Ever since that first class, I’ve been hooked.  I’ve been coding and improving my skills every day.  Or in the words of my teacher, “ABC, always be coding” .')
+    var storyParagraph2=$("<p>").append('So I started take Code Academy classes. And I began to realize I might be able to coding further . A few months later, a friend  mentioned he had seen an ad for a Rutgers Coding Bootcamp, and I was immediately intrigued. I had looked at other bootcamps before, but this one was different. I told my friend "I needed to think about it."')
+    var storyParagraph3=$("<p>").append('By the end of the next week I had signed up for the bootcamp. A month later, I went to the first class. Ever since that first class, I’ve been hooked. I’ve been coding and improving my skills every day. Or in the words of my teacher, “ABC, always be coding” .')
+    
         var panel= $("<div>").addClass("panel panel-default")
     var panelbody = $("<div>").addClass("panel-body")
     var storyID =$("<div>").addClass("storyID")
@@ -47,7 +47,6 @@ $("document").ready(function(){
         $(panelbody).append(storyParagraph1)
     .append(storyParagraph2)
     .append(storyParagraph3)
-    .append(storyParagraph4)
     $(panel).append(panelbody);
     $(".target").append(storyHeadline)
     .append(strangelove)
