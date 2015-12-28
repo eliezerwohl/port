cf4d4d847b02f907f5350d6bd496af45cf9cfcf0
eliezerwohl <eliezerwohl@gmail.com>
2015-12-28 16:26:40 -0500

    added project links

--- javascript/animation.js
+++ javascript/animation.js
@@ -13,9 +13,26 @@ $(document).ready(function(){
 window.open('http://stackoverflow.com/users/5510690/eliezer-wohl')
 });
 
-  $(document).on("click", ".github", function(){
-  window.open('https://github.com/eliezerwohl') 
+  $(document).on("click", ".john", function(){
+  window.open('https://evening-beyond-7392.herokuapp.com') 
 });
+
+  $(document).on("click", ".toDo", function(){
+  window.open('https://fathomless-cliffs-4838.herokuapp.com') 
+});
+
+  $(document).on("click", ".triHotel", function(){
+  window.open('https://polar-inlet-6927.herokuapp.com') 
+});
+
+  $(document).on("click", ".RPS", function(){
+  window.open(' https://polar-inlet-6927.herokuapp.com') 
+});
+
+  $(document).on("click", ".toGo", function(){
+  window.open('http://to-go-list.herokuapp.com') 
+});
+
 
   });
 
