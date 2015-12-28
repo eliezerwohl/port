918a30caedd4210c8702fbaeab17c0716e94689f
eliezerwohl <eliezerwohl@gmail.com>
2015-12-27 21:34:14 -0500

    added testimonial, added music, linked bottom navbar"

--- javascript/commits.js
+++ javascript/commits.js
@@ -1,40 +1,40 @@
-$(document).ready(function(){
-  $(document).on("click", "a", function(e){
-    e.preventDefault();
+// $(document).ready(function(){
+//   $(document).on("click", "a", function(e){
+//     e.preventDefault();
 
-    $.ajax({
-      type: "GET",
-       url: $(this).attr("href"),
-      // console.log($this);
-success: function(commits){
-        $("tbody").empty();
+//     $.ajax({
+//       type: "GET",
+//        url: $(this).attr("href"),
+//       // console.log($this);
+// success: function(commits){
+//         $("tbody").empty();
 
-        for(var i = 0; i < commits.length; i++){
+//         for(var i = 0; i < commits.length; i++){
 
-          $("tbody").append(buildTableRow(commits[i]));
-        }
-      },
-      error: function(jqXHR, textStatus, errorThrown){
-        console.log(jqXHR);
-        console.log(textStatus);
-        console.log(errorThrown);
-      }
-    });
+//           $("tbody").append(buildTableRow(commits[i]));
+//         }
+//       },
+//       error: function(jqXHR, textStatus, errorThrown){
+//         console.log(jqXHR);
+//         console.log(textStatus);
+//         console.log(errorThrown);
+//       }
+//     });
 
-    function buildTableRow(commitData){
+//     function buildTableRow(commitData){
 
-      var authorTd = $("<td>").append(commitData.author.login);
-      var messageTd = $("<td>").append(commitData.commit.message);
-      var dateTd = $("<td>").append(commitData.commit.author.date);
-      var htmlUrl =$("<a>").attr("href", commitData.html_url).append("<td>").append(commitData.html_url)
+//       var authorTd = $("<td>").append(commitData.author.login);
+//       var messageTd = $("<td>").append(commitData.commit.message);
+//       var dateTd = $("<td>").append(commitData.commit.author.date);
+//       var htmlUrl =$("<a>").attr("href", commitData.html_url).append("<td>").append(commitData.html_url)
 
-      var commitLink =$("<tr>") 
+//       var commitLink =$("<tr>") 
     
-        .append(authorTd)
-        .append(messageTd)
-        .append(dateTd)
-        .append(htmlUrl);
-        return commitLink
-    }
-  });
-});
+//         .append(authorTd)
+//         .append(messageTd)
+//         .append(dateTd)
+//         .append(htmlUrl);
+//         return commitLink
+//     }
+//   });
+// });
