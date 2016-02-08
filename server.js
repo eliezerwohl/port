6608109bc8227769da1766d8e2890a5ea58f1406
eliezerwohl <eliezerwohl@gmail.com>
2016-02-08 14:24:29 -0500

    added a function in server.js that you fill oout the guest book, it redirects you to a message saying that you've entered in a message in teh guest book

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 8080;
+bodyParser= require('body-parser')
 
+
+app.use(bodyParser.urlencoded({extended:false}));
 app.use("/javascript", express.static("public/javascript"));
 app.use("/css", express.static("public/css"));
 app.use("/img", express.static("public/img"));
@@ -16,7 +19,24 @@ app.get("/clicky", function(req, res) {
   res.sendFile(process.cwd() + "/public/html/clicky.html");
 });
 
+app.get('/user/:userName', function(req, res) {
+ console.log (req.params.userName)
+res.send(req.params.userName)
+});
 
+
+app.get('/guestbook', function(req, res) {
+res.sendFile(process.cwd() + "/public/html/guestbook.html"); 
+});
+
+app.post('/guestbook', function(req, res) {
+res.redirect("/guestBookMessage");
+
+});
+
+app.get("/guestBookMessage", function(req, res){
+  res.sendFile(process.cwd() + "/public/html/guestMessage.html")
+});
 
 app.get("/rps", function(req, res) {
   res.sendFile(process.cwd() + "/public/html/rps.html");
